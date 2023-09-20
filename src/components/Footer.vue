dfb6d0bfe673095db1fc3b84ed27308958023dc1
adetoyato <alexis.detoyato@newbrighton.edu.ph>
2023-09-20 17:40:54 +0800

    update

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer>
+    <div class="container">
+        <div class="left">
+            <div class="col-1">
+                <router-link class="header" :to="{ name: 'home' }"> Lou Geh Travel </router-link>
+                <label></label>
+            </div>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
